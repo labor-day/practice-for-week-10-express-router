7252c7ebfa6f1210ad946f19d731e304c729c066
labor-day <shaffer.scott@yahoo.com>
2022-08-10 20:07:39 -0600

    added extra route handlers

--- server/routes/colors.js
+++ server/routes/colors.js
@@ -10,4 +10,12 @@ router.get('/:name', (req, res) => {
   res.json(`GET /colors/:${req.params.name}`);
 });
 
+router.post('/:name/css-styles', (req, res) => {
+  res.json(`POST /colors/:${req.params.name}/css-styles`);
+});
+
+router.delete('/:name/css-styles/:style', (req, res) => {
+  res.json(`DELETE /colors/:${req.params.name}/css-styles/:${req.params.style}`);
+});
+
 module.exports = router;
